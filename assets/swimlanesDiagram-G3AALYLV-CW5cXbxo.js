da4c381e877efe2a39f38719d7fe7f015ebfb321
Piotr Dudzic <piotrdudzic@Piotrs-MacBook-Air.local>
2026-07-29 23:30:19 +0200

    Rebuild dist

---
+++ assets/swimlanesDiagram-G3AALYLV-CW5cXbxo.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-yA6A9Qc2.js";import{_ as a}from"./index-Bf6tbgeH.js";import"./chunk-5VM5RSS4-WTCDhyIa.js";import"./chunk-XXDRQBXY-BxiqMwMt.js";import"./chunk-VR4S4FIN-BLIXGvFf.js";import"./chunk-32BRIVSS-vOX5aBXE.js";import"./channel-CQoV4WuT.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
