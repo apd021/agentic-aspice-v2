e233c5c0ae426da26ae67025c09bac3e96f10f54
Piotr Dudzic <piotrdudzic@Piotrs-MacBook-Air.local>
2026-07-29 22:36:31 +0200

    Add dist for GitHub Pages

---
+++ dist/assets/swimlanesDiagram-G3AALYLV-znAcV50I.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-3SH_C1VP.js";import{_ as a}from"./index-BsljY2Xo.js";import"./chunk-5VM5RSS4-BLV019x2.js";import"./chunk-XXDRQBXY-Cdn8oQO_.js";import"./chunk-VR4S4FIN-CSQoFnxE.js";import"./chunk-32BRIVSS-Cxc6VRLF.js";import"./channel-BRC-b3bF.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
