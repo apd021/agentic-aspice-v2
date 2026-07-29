a354069f7948b4fcc0aa1073d83b2b89751bd32a
Piotr Dudzic <piotrdudzic@Piotrs-MacBook-Air.local>
2026-07-29 22:45:10 +0200

    Add .nojekyll for GitHub Pages

---
+++ dist/assets/swimlanesDiagram-G3AALYLV-XM7gRmUg.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-DOaxUIrd.js";import{_ as a}from"./index-Cu1x363N.js";import"./chunk-5VM5RSS4-Ch2SWZJT.js";import"./chunk-XXDRQBXY-DPyCN62g.js";import"./chunk-VR4S4FIN-Bp3orr9M.js";import"./chunk-32BRIVSS-SWm8-1dK.js";import"./channel-Cv5K54Wl.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
